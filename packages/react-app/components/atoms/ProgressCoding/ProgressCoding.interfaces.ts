fb765d509261aa8fd5f2ca8cb3c40498805db9ea
dany <dannylasso0909@hotmail.com>
2023-12-03 14:00:47 -0500

    build pages coding, /coding/score, /records

--- packages/react-app/components/atoms/ProgressCoding/ProgressCoding.interfaces.ts
+++ packages/react-app/components/atoms/ProgressCoding/ProgressCoding.interfaces.ts
@@ -1,3 +1,4 @@
 export interface IProgressCodingProps {
     value: number;
+    color: string
 }
--- packages/react-app/components/atoms/ProgressCoding/index.tsx
+++ packages/react-app/components/atoms/ProgressCoding/index.tsx
@@ -1,10 +1,11 @@
-import { Progress } from "@material-tailwind/react";
 import { IProgressCodingProps } from "./ProgressCoding.interfaces";
 
-export default function ProgressCoding({value}: IProgressCodingProps) {
-    return (
-        <div className="flex w-full flex-col gap-4">
-          <Progress value={value} variant="gradient" />
-        </div>
-      );
+export default function ProgressCoding({ value, color }: IProgressCodingProps) {
+  return (
+    <div className="basis-5/6 px-3 mx-4">
+      <div className="h-1 w-full bg-neutral-200 dark:bg-neutral-600 ">
+        <div className="h-5 bg-primary text-center text-white rounded-lg font-semibold" style={{ width: `${value}%`, background: color }}> {value}% Completado</div>
+      </div>
+    </div>
+  );
 } 
