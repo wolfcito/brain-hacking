fb66c9526e8e1bd0f98a0e8b51fd8dd8e0aaf008
Luis Fernando Ushiña <guffenix@gmail.com>
2023-12-03 14:54:49 -0500

    feat: donation integrated with blockchain (#8)
    * chore: abi scripts added
    
    * chore: sm donation integration
    
    * chore: readme project updated
    
    * feat: canstants for donation sm
    
    * chore: form donate
    
    * chore: improve suggestion donation
    
    * chore: improve classname
    
    * feat: send donation implementation
    
    * chore: sm method implementation

--- packages/react-app/components/atoms/SuggestionDonation/index.tsx
+++ packages/react-app/components/atoms/SuggestionDonation/index.tsx
@@ -1,10 +1,15 @@
-import { ISuggestionDonation } from "./SuggestionDonation.interface";
+import { ISuggestionDonation } from './SuggestionDonation.interface'
 
-export default function SuggestionDonation({ label = "Aportar al proyecto", onClick }: ISuggestionDonation) {
-    return (
-        <div className="flex flex-row w-2/6 my-6">
-            <h2 className="basis-3/4 my-1">{label}</h2>
-            <button onClick={onClick} className="basis-1/4">Donar</button>
-        </div>
-    )
+export default function SuggestionDonation({
+  label = '¿Te gustaría ayudar a la Humanidad?',
+  label2 = 'Tu generosidad puede cambiar vidas.',
+  onClick,
+}: ISuggestionDonation) {
+  return (
+    <div className="my-6 flex w-full flex-col items-center justify-center">
+      <h2 className="my-1">{label}</h2>
+      <span className="my-1">{label2}</span>
+      {/* <button onClick={onClick} className="basis-1/4">Donar</button> */}
+    </div>
+  )
 }
