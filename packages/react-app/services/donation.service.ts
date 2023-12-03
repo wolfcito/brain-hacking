13529d3d5b712e0181744fdc8ae8c9058f398bcd
Luis Fernando <guffenix@gmail.com>
2023-12-03 14:38:21 -0500

    chore: sm method implementation

--- packages/react-app/services/donation.service.ts
+++ packages/react-app/services/donation.service.ts
@@ -1,15 +1,46 @@
-import { Contract, providers } from 'ethers'
+import { Contract, ethers, utils } from 'ethers'
 import donationAbi from '@/scripts/donations.abi.json'
-import { useMemo } from 'react'
+import { DONATION_ADDRESS } from '@/constants/wallet'
 
-const donationAddress = '0x6490499a3D79cc39f69B6D06163d359eb6eaF2fA'
-const providerURL = 'https://alfajores-forno.celo-testnet.org'
+export const getDonorBalance = async (address: string) => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const contract = new Contract(DONATION_ADDRESS, donationAbi, provider)
 
-const provider = new providers.JsonRpcProvider(providerURL)
+  const balance = await contract.donorBalances(address)
+  return utils.formatEther(balance)
+}
 
-export const useDonationContract = () => {
-  const active = true
-  return useMemo(() => {
-    if (active) return new Contract(donationAddress, donationAbi, provider)
-  }, [active])
+export const donateToContract = async (amount: number) => {
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const contract = new Contract(DONATION_ADDRESS, donationAbi, provider)
+
+    const signer = provider.getSigner()
+    console.log('signer', signer)
+    console.log('amount', amount)
+    const contractWithSigner = contract.connect(signer)
+
+    const transaction = await contractWithSigner.donate({
+      value: utils.parseEther(amount.toString()),
+    })
+
+    await transaction.wait()
+
+    console.log('Donation successful!')
+  } catch (e) {
+    console.log('error', e)
+  }
+}
+
+export const withdrawFromContract = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const contract = new Contract(DONATION_ADDRESS, donationAbi, provider)
+
+  const signer = provider.getSigner()
+  const contractWithSigner = contract.connect(signer)
+
+  const transaction = await contractWithSigner.withdraw()
+  await transaction.wait()
+
+  console.log('Withdrawal successful!')
 }
